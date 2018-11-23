6a4fc593fd17bd629568f1de20a1e2fdb5e54792
Miguel Sanchez <misan@disca.upv.es>
2018-11-23 17:27:19 +0100

    Various ways to generate a Morse-code signal
    Code for Arduino

---
+++ sos/sos.ino
@@ -0,0 +1,24 @@
+int tiempo=200;
+int pin=13;
+void setup() {
+  pinMode(pin,OUTPUT);
+}
+void tres(int duracion) {
+for( int i=1 ; i<4  ; i++ ) // repetir 3 veces
+  {
+    digitalWrite(pin,HIGH); 
+    delay(duracion*tiempo);
+    digitalWrite(pin,LOW);
+    delay(tiempo);
+  }
+}
+void loop() {
+  // put your main code here, to run repeatedly:
+  tres(1);
+  delay(2*tiempo);
+  tres(3);
+  delay(2*tiempo); // tres(3);
+  tres(1);  
+  delay(4*tiempo); 
+  //while(true) {};   // bucle infinito -- endless loop
+}
---
+++ sos2/sos2.ino/sos2.ino.ino
@@ -0,0 +1,13 @@
+int tiempo[18]={1,1,1,1,1,3,3,1,3,1,3,3,1,1,1,1,1,5}; 
+int pin=13;
+
+void setup() {
+  pinMode(pin,OUTPUT);
+}
+
+void loop() {
+ for(int i=0; i<18; i++ ) {
+  digitalWrite(pin, (i % 2 == 0) ); 
+  delay(tiempo[i]*20); 
+ }
+}
